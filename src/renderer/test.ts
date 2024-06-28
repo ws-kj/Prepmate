7318f5b9bdce8d19136d433c19b4cc8a424a2749
Will <wsavage6316@gmail.com>
2024-06-28 15:03:07 -0400

    test parsing

--- src/renderer/test.ts
+++ src/renderer/test.ts
@@ -1,6 +1,9 @@
-import { Test, Question, Answer } from './types';
-import { parse } from 'csv-parse/sync';
+import { Test, Question, Answer, QuestionType } from './types';
 import { ElectronHandler } from '../main/preload';
+import { usePapaParse } from 'react-papaparse';
+import { parse } from 'path';
+
+const { readString } = usePapaParse();
 
 declare global {
   interface Window {
@@ -8,21 +11,79 @@ declare global {
   }
 }
 
+interface TestCsvRow {
+  Section: string,
+  Question: string,
+  Passage: string,
+  A: string,
+  B: string,
+  C: string,
+  D: string,
+  Correct: string,
+  Categories: string,
+}
+
+const choiceLetters: string[] = ["A", "B", "C", "D"];
+
 export const loadTest = (path: string): Test | null => {
   try {
-    const file = window.electron.fileSystem.readFile(path);
-    console.log(file);
+    window.electron.fileSystem.readFile(path).then((file) => {
+      readString(file, {
+        header: true,
+        complete: (results) => {
+          const test: Test = {
+            questions: [],
+            answers: []
+          };
+          const rows: TestCsvRow[] = results.data as TestCsvRow[];
+          rows.forEach((row: TestCsvRow, i: number) => {
+            const section: number = parseInt(row["Section"]);
+            const type: QuestionType = section > 2 ? "math" : "reading";
+            const passage: string | null = !row["Passage"] ? null : row["Passage"];
+            const question: string = row["Question"];
+
+            var choices: string[] | null = null;
+            var correct: number | null = null;
+            var freeResponse: string | null = row["Correct"];
+
+            if(row["A"] && row["B"] && row["C"] && row["D"]
+                && choiceLetters.indexOf(row["Correct"]) != -1) {
+              choices = [row["A"], row["B"], row["C"], row["D"]];
+              correct = choiceLetters.indexOf(row["Correct"]);
+              freeResponse = null;
+            }
+
+            const categories: string[] = row["Categories"]
+              .split(",")
+              .map(word => word.trim())
+              .filter(word => word.length > 0);
+
+            test.questions.push({
+              id: i,
+              section: section,
+              type: type,
+              passage: passage,
+              question: question,
+              choices: choices,
+              categories: categories
+            });
+
+            test.answers.push({
+              id: i,
+              choice: correct,
+              freeResponseRaw: freeResponse
+            });
+          });
+
+          console.log(test);
+          return test;
+        },
+      });
+
+    });
   } catch (error) {
     console.log(error);
     return null;
   }
-/*
-  const records = parse(file, {
-    columns: true,
-    skipEmptyLines: true
-  });
-
-  console.log(records);
-*/
   return null;
 }
